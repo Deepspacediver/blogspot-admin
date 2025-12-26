102819957f48ebf38bcbbe368cb72e4276031628
lukasz-mac <luka.koscik@gmail.com>
2025-12-26 19:26:38 +0100

    feat: add queryClient with provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./lib/query.config";
+
 function App() {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <></>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
