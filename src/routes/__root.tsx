2eb91e1eda2cf09e8fa9113023939de980242048
lukasz-mac <luka.koscik@gmail.com>
2026-01-02 19:52:04 +0100

    feat: replace createRootRoute with context root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,11 @@
+import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
   component: RootComponent,
 });
 
