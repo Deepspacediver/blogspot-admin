90379054b5d556b1b82bd4edf0e8df26f19af823
lukasz-mac <luka.koscik@gmail.com>
2026-01-02 20:32:45 +0100

    fix: update imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import "./reset.css";
+import "@/reset.css";
+import "@/index.css";
 
-import queryClient from "./lib/query.config.ts";
+import queryClient from "@/lib/query.config.ts";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./router.ts";
+import { router } from "@/router.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
