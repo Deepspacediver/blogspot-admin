afa5cbf7091d64869911d9569ce4538339534096
lukasz-mac <luka.koscik@gmail.com>
2026-01-07 19:25:25 +0100

    feat: add router link & external link components

---
+++ src/components/ui/link.tsx
@@ -0,0 +1,58 @@
+import {
+  Link as RouterLink,
+  type LinkComponentProps,
+} from "@tanstack/react-router";
+import type { VariantProps } from "class-variance-authority";
+import { type AnchorHTMLAttributes, type ReactNode } from "react";
+import { buttonVariants } from "./button-variants";
+import { cn } from "@/lib/utils";
+
+type LinkProps = VariantProps<typeof buttonVariants> &
+  LinkComponentProps & {
+    asChild?: boolean;
+  };
+
+export function Link({
+  children,
+  className,
+  size = "default",
+  variant = "link",
+  ...props
+}: LinkProps) {
+  return (
+    <RouterLink
+      className={cn(
+        buttonVariants({
+          variant,
+          size,
+        }),
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </RouterLink>
+  );
+}
+
+type ExternalLinkProps = VariantProps<typeof buttonVariants> &
+  AnchorHTMLAttributes<HTMLAnchorElement> & {
+    children: ReactNode;
+  };
+
+export function ExternalLink({
+  href,
+  children,
+  className,
+  variant,
+  size,
+  ...props
+}: ExternalLinkProps) {
+  <a
+    href={href}
+    className={cn(buttonVariants({ variant, size }), className)}
+    {...props}
+  >
+    {children}
+  </a>;
+}
