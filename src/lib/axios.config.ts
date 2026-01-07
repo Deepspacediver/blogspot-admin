b967a05dd66d06693ae828208261b0a2c01e6db3
lukasz-mac <luka.koscik@gmail.com>
2026-01-07 19:26:58 +0100

    feat(experimental): add retry logic when expired access token

--- src/lib/axios.config.ts
+++ src/lib/axios.config.ts
@@ -1,8 +1,33 @@
+import { refreshToken } from "@/api/auth/fetch";
 import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
+
+// TODO handle multiple requests
+axiosInstance.interceptors.response.use(
+  function onFulfilled(response) {
+    return response;
+  },
+  async function onRejected(error) {
+    const originalRequest = error.config;
+
+    const responseStatus = (error?.response?.status || error?.status) as
+      | number
+      | undefined;
+    const isUnauthorized = responseStatus === 401;
+    const wasRequestRetried = !!originalRequest._retried;
+
+    if (isUnauthorized && !wasRequestRetried) {
+      originalRequest._retried = true;
+      await refreshToken();
+      return axiosInstance(originalRequest);
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
