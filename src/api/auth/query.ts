a9bc44f4503d4e219a3c70e450fcf6b79b67444c
lukasz-mac <luka.koscik@gmail.com>
2026-01-11 15:29:02 +0100

    fix: use router navigate instead redirect

--- src/api/auth/query.ts
+++ src/api/auth/query.ts
@@ -1,28 +1,26 @@
 import { useMutation } from "@tanstack/react-query";
 import { signIn, signUp } from "@/api/auth/fetch";
-import { redirect } from "@tanstack/react-router";
+import { useNavigate } from "@tanstack/react-router";
 import { setIsLoggedIn } from "@/lib/local-storage-helpers";
 
 export const useSignUp = () => {
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: signUp,
     onSuccess: () => {
       setIsLoggedIn(true);
-      throw redirect({
-        to: "/",
-      });
+      navigate({ to: "/" });
     },
   });
 };
 
 export const useSignIn = () => {
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: signIn,
     onSuccess: () => {
       setIsLoggedIn(true);
-      throw redirect({
-        to: "/",
-      });
+      navigate({ to: "/" });
     },
   });
 };
