ecf341fb05951284aeddcfbdf71bca5451ae6fbf
lukasz-mac <luka.koscik@gmail.com>
2026-01-21 21:10:35 +0100

    feat: add <FormMessage/>

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,11 +11,25 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
         "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
+        className,
       )}
       {...props}
     />
   );
 }
 
-export { Input };
+type FormMessageProps = { message?: string } & React.ComponentProps<"p">;
+
+function FormMessage({ message, className, ...props }: FormMessageProps) {
+  if (!message) {
+    return null;
+  }
+
+  return (
+    <p className={cn("text-red-500 text-xs", className)} {...props}>
+      {message}
+    </p>
+  );
+}
+
+export { Input, FormMessage };
