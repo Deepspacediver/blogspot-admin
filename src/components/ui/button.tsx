303f5254ec5af8fe3cde9feb063a5671777d5f10
lukasz-mac <luka.koscik@gmail.com>
2026-01-21 21:09:54 +0100

    feat: add loading state for button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,16 +3,21 @@ import { Slot } from "@radix-ui/react-slot";
 import { type VariantProps } from "class-variance-authority";
 import { buttonVariants } from "@/components/ui/button-variants";
 import { cn } from "@/lib/utils";
+import { LoaderCircle } from "lucide-react";
 
 function Button({
   className,
   variant = "default",
   size = "default",
   asChild = false,
+  isLoading = false,
+  disabled,
+  children,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
+    isLoading?: boolean;
   }) {
   const Comp = asChild ? Slot : "button";
 
@@ -22,8 +27,11 @@ function Button({
       data-variant={variant}
       data-size={size}
       className={cn(buttonVariants({ variant, size, className }))}
+      disabled={isLoading || disabled}
       {...props}
-    />
+    >
+      {!isLoading ? children : <LoaderCircle className="animate-spin size-5" />}
+    </Comp>
   );
 }
 
