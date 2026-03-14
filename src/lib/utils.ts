49ee19b7482957a3a89b7b4c48c6615cd4d57df7
lukasz-mac <luka.koscik@gmail.com>
2026-03-14 10:00:56 +0100

    feat: add date formatting helpers with date-fns

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { format } from "date-fns";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatDateToAppConvention = (data: Date | string | number) => {
+  return format(new Date(data), "MMMM d, yyyy");
+};
+
+export const formatDateToDateTimeAttribute = (data: Date | string | number) => {
+  return format(new Date(data), "yyyy-MM-dd kk:mm:ss");
+};
+
+export const getFormattedDateWithAttribute = (data: Date | string | number) => {
+  const attributeDate = formatDateToDateTimeAttribute(data);
+  const formattedDate = formatDateToAppConvention(data);
+  return {
+    attributeDate,
+    formattedDate,
+  };
+};
