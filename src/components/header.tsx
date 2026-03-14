10358bdb4d11e3fbc060270262ad1effda1409ef
lukasz-mac <luka.koscik@gmail.com>
2026-03-14 10:02:33 +0100

    feat: rework UserProfile to handle static and dynamic users

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,31 @@
+import { useLocation, useNavigate } from "@tanstack/react-router";
 import SignOutButton from "./sign-out.button";
 import { Button } from "./ui/button";
 import UserProfile from "./user-profile";
 import { Suspense } from "react";
+import { Link } from "./ui/link";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isPostCreatePath = location.href === "/posts/create";
   return (
-    <header>
-      <nav>
-        <Button variant={"link"}>d</Button>
+    <header className="flex items-center p-2">
+      <nav className="mr-auto">
+        <Link to="/posts/create">Home</Link>
       </nav>
       <Suspense fallback={<div>Loading user...</div>}>
-        <UserProfile />
+        {!isPostCreatePath && (
+          <Button
+            className="mr-2"
+            onClick={() => {
+              navigate({ to: "/posts/create" });
+            }}
+          >
+            New post
+          </Button>
+        )}
+        <UserProfile className="text-sm" />
         <SignOutButton />
       </Suspense>
     </header>
--- src/components/user-profile.tsx
+++ src/components/user-profile.tsx
@@ -1,18 +1,85 @@
+import type { UserDetailsReturn } from "@/api/users/fetch";
 import { useUser } from "@/api/users/query";
+import { cn } from "@/lib/utils";
+import type { User } from "@/types";
 import { User as UserSVG } from "lucide-react";
 
-export default function UserProfile() {
-  const { data: user } = useUser();
-  const { pictureUrl, username, email } = user;
+type UnLoggedUserProfileProps = {
+  data: Omit<UserDetailsReturn, "role"> & { role?: User["role"] | null };
+  children?: React.ReactNode;
+  className?: string;
+};
+export function UserContent({
+  data,
+  children,
+  className,
+}: UnLoggedUserProfileProps) {
+  const { pictureUrl, username, email } = data;
+  return (
+    <div className={cn("flex gap-1 items-center", className)}>
+      <div
+        className={cn(
+          "rounded-full overflow-hidden ",
+          !pictureUrl && "bg-secondary",
+        )}
+      >
+        {pictureUrl ? (
+          <img src={pictureUrl} width={24} height={24} alt="user picture" />
+        ) : (
+          <UserSVG className="size-10 p-1" />
+        )}
+      </div>
+      <div className="flex flex-col">
+        <p>{username || email}</p>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export function UnLoggedUserProfile({
+  data,
+  children,
+  className,
+}: UnLoggedUserProfileProps) {
+  return (
+    <UserContent className={className} data={data}>
+      {children}
+    </UserContent>
+  );
+}
+
+export function LoggedUserProfile({
+  children,
+  className,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) {
+  const {
+    data: { pictureUrl, username, email },
+  } = useUser();
 
   return (
-    <div className="flex gap-1">
-      {pictureUrl ? (
-        <img src={pictureUrl} width={24} height={24} alt="user picture" />
-      ) : (
-        <UserSVG className="size-6" />
-      )}
-      <p>{username || email}</p>
-    </div>
+    <UserContent className={className} data={{ pictureUrl, username, email }}>
+      {children}
+    </UserContent>
+  );
+}
+
+export default function UserProfile({
+  data,
+  children,
+  className,
+}: Partial<UnLoggedUserProfileProps>) {
+  if (data) {
+    return (
+      <UnLoggedUserProfile className={className} data={data}>
+        {children}
+      </UnLoggedUserProfile>
+    );
+  }
+  return (
+    <LoggedUserProfile className={className}>{children}</LoggedUserProfile>
   );
 }
