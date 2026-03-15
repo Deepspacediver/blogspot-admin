513c2e4548242cd85c4cb08da6bf498c5f3d1d7a
lukasz-mac <luka.koscik@gmail.com>
2026-03-15 09:25:36 +0100

    fix: add correct href to home link in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
   return (
     <header className="flex items-center p-2">
       <nav className="mr-auto">
-        <Link to="/posts/create">Home</Link>
+        <Link to="/">Home</Link>
       </nav>
       <Suspense fallback={<div>Loading user...</div>}>
         {!isPostCreatePath && (
