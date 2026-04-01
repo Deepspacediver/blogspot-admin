c53d214b0e5489faad66ebbfaa83b6f79b22aace
lukasz-mac <luka.koscik@gmail.com>
2026-04-01 20:35:15 +0200

    feat: add formatDateDistance helper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
-import { format } from "date-fns";
+import { format, formatDistanceToNowStrict } from "date-fns";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -14,11 +14,19 @@ export const formatDateToDateTimeAttribute = (data: Date | string | number) => {
   return format(new Date(data), "yyyy-MM-dd kk:mm:ss");
 };
 
+export const formatDateDistance = (data: Date | string | number) => {
+  return formatDistanceToNowStrict(new Date(data), {
+    addSuffix: true,
+  });
+};
+
 export const getFormattedDateWithAttribute = (data: Date | string | number) => {
   const attributeDate = formatDateToDateTimeAttribute(data);
   const formattedDate = formatDateToAppConvention(data);
+  const distanceDate = formatDateDistance(data);
   return {
     attributeDate,
     formattedDate,
+    distanceDate
   };
 };
