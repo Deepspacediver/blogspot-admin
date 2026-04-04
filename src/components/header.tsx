04be91f2c7b65073eb04aeb09e8b4619e3385b44
lukasz-mac <luka.koscik@gmail.com>
2026-04-04 11:07:38 +0200

    feat: add Skeleton loader for <Header/>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,17 +4,33 @@ import { Button } from "./ui/button";
 import UserProfile from "./user-profile";
 import { Suspense } from "react";
 import { Link } from "./ui/link";
+import { Skeleton } from "./ui/skeleton";
+
+function HeaderSkeleton({ isPostCreatePath }: { isPostCreatePath?: boolean }) {
+  return (
+    <div className="flex items-center">
+      {!isPostCreatePath && <Skeleton className="mr-2 w-24 h-10" />}
+      <div className="flex gap-1 items-center mx-2">
+        <Skeleton className="size-6 rounded-full shrink-0" />
+        <Skeleton className="h-4 w-32" />
+      </div>
+      <Skeleton className="mx-4 w-24 h-10" />
+    </div>
+  );
+}
 
 export default function Header() {
   const navigate = useNavigate();
   const location = useLocation();
   const isPostCreatePath = location.href === "/posts/create";
   return (
-    <header className="flex items-center p-2">
+    <header className="flex items-center p-2 h-14">
       <nav className="mr-auto">
         <Link to="/">Home</Link>
       </nav>
-      <Suspense fallback={<div>Loading user...</div>}>
+      <Suspense
+        fallback={<HeaderSkeleton isPostCreatePath={isPostCreatePath} />}
+      >
         {!isPostCreatePath && (
           <Button
             className="mr-2"
